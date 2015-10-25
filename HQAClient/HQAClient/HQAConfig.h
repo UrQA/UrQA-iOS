2f90eb9e3a18ad75a8a1cdcd8e414a3efc0d7152
Sunghoon Kang <devholic@plusquare.com>
2015-10-25 18:31:03 +0900

    Update HQAConfig.h

--- HQAClient/HQAClient/HQAConfig.h
+++ HQAClient/HQAClient/HQAConfig.h
@@ -61,7 +61,7 @@
 
 // Definition
 #define HQA_VERSION            @"0.0.1"
-#define HQA_DOMAIN             @"http://devapi.honeyqa.io:8080/api/ios"
+#define HQA_DOMAIN             @"http://api.honeyqa.io/api/ios"
 #define HQA_REQUST_TYPE        @"JSON"
 
 // Console Logging
